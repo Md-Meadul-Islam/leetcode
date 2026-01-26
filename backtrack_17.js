3872f0026e6c37c5d3e9ce68d92cdf6d1214ba9f
Md Meadul Islam <mdmeadulislam@gmail.com>
2026-01-26 15:17:14 +0600

    rename

--- backtrack_17.js
+++ backtrack_17.js
@@ -1,3 +1,4 @@
+//https://leetcode.com/problems/letter-combinations-of-a-phone-number?envType=problem-list-v2&envId=backtracking
 var letterCombinations = function (digits) {
   const map = {
     2: ["a", "b", "c"],
@@ -31,34 +32,34 @@ var letterCombinations = function (digits) {
 console.log(letterCombinations("92"));
 
 function letterCombinations2(digits) {
-    if (digits.length === 0) return [];
-    
-    const phoneMap = {
-        '2': 'abc',
-        '3': 'def',
-        '4': 'ghi',
-        '5': 'jkl',
-        '6': 'mno',
-        '7': 'pqrs',
-        '8': 'tuv',
-        '9': 'wxyz'
-    };
-    
-    const result = [];
-    
-    function backtrack(index, current) {
-        if (index === digits.length) {
-            result.push(current);
-            return;
-        }
-        
-        const letters = phoneMap[digits[index]];
-        for (let letter of letters) {
-            backtrack(index + 1, current + letter);
-        }
+  if (digits.length === 0) return [];
+
+  const phoneMap = {
+    2: "abc",
+    3: "def",
+    4: "ghi",
+    5: "jkl",
+    6: "mno",
+    7: "pqrs",
+    8: "tuv",
+    9: "wxyz",
+  };
+
+  const result = [];
+
+  function backtrack(index, current) {
+    if (index === digits.length) {
+      result.push(current);
+      return;
     }
-    
-    backtrack(0, '');
-    console.log(result)
-    return result;
+
+    const letters = phoneMap[digits[index]];
+    for (let letter of letters) {
+      backtrack(index + 1, current + letter);
+    }
+  }
+
+  backtrack(0, "");
+  console.log(result);
+  return result;
 }
